ee1a3b8bdb142139c586fc84897a5c02c46d79b7
ada <81255414+adaGrad1@users.noreply.github.com>
2023-04-11 02:12:24 -0400

    added first attempt at language supervision

--- supervised_bvae.py
+++ supervised_bvae.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torch.linalg as linalg
 from betavae import BVAE
 
 class SupervisedBVAE(BVAE):
@@ -15,3 +16,15 @@ class SupervisedBVAE(BVAE):
         img = super().to_img(z)
         yhat = self.z_to_out_map(z)
         return img, mu, var, z, yhat
+    
+    def forward_with_supervision(self, x, y):
+        mu, var = super().to_parameters(x)
+        
+        z = mu
+        yhat = self.z_to_out_map(z)
+        diff = F.linear((y-yhat), linalg.pinv(self.z_to_out_map.weight))
+        z = z + diff # it will not necessarily be the case that Wz+bias = y
+        
+        
+        img = super().to_img(z)
+        return img
