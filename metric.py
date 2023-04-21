78ea8d3bb38839e4fc55bc2c85a66506d7f69f68
Aboli Marathe <47828335+Infernolia@users.noreply.github.com>
2023-04-21 04:18:48 -0400

    Update metric.py

--- metric.py
+++ metric.py
@@ -1,5 +1,6 @@
 from image_similarity_measures.evaluate import evaluation
 from PIL import Image, ImageOps
+import cv2
 
 def metricx(predicted_image_path,gt_image_path):
     # predicted_image_path = "C:/Users/DELL/Desktop/Project/Project/Output/Output_1.png"
@@ -8,6 +9,23 @@ def metricx(predicted_image_path,gt_image_path):
 
   MM = ["issm","psnr","rmse","sam","sre","ssim"]
   res = []
+
+  image_A = cv2.imread(gt_image_path).astype("float32")
+
+  image_B = cv2.imread(predicted_image_path).astype("float32")
+
+  image_A = cv2.cvtColor(image_A,cv2.COLOR_BGR2LAB) 
+  image_B = cv2.cvtColor(image_B,cv2.COLOR_BGR2LAB) 
+
+  diff = cv2.add(image_A,-image_B)
+
+  diff_L = diff[:,:,0]
+  diff_A = diff[:,:,1]
+  diff_B = diff[:,:,2]
+
+  delta_e = np.mean( np.sqrt(diff_L*diff_L + diff_A*diff_A + diff_B*diff_B) )
+
+  res.append(delta_e)
     
   for m in MM:    
     e = evaluation(org_img_path=gt_image_path, pred_img_path=predicted_image_path, metrics=[m])
@@ -27,5 +45,11 @@ def metricx(predicted_image_path,gt_image_path):
   for m in MM:
     e = evaluation(org_img_path=gt_image_path,  pred_img_path=predicted_image_path,  metrics=[m])
     res.append(e[m])
+  
+  
+
+
+
+
 
   return res
